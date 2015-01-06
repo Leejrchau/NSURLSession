80fc40447c11d164f60ed0c0223ff51b82854133
lizhichao <lizhichao@huainianxueshengshidaiMacBook-Pro.local>
2015-01-06 19:48:23 +0800

    Initial Commit

---
+++ NSURLSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSession
+//
+//  Created by lizhichao on 15/1/6.
+//  Copyright (c) 2015年 com.example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSession
+//
+//  Created by lizhichao on 15/1/6.
+//  Copyright (c) 2015年 com.example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
